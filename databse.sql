8c1c69c4a540e513488747b5b65a1597126e6c39
Swaka500 <derrickswaka910@gmail.com>
2025-05-10 19:59:23 +0300

    Done

--- databse.sql
+++ databse.sql
@@ -1,1 +1,37 @@
+-- Below is the products Table
+CREATE TABLE Products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT,
+    price DECIMAL(30, 10),
+    quantity_in_stock INT NOT NULL
+);
+
+-- Suppliers Table
+CREATE TABLE Suppliers (
+    supplier_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    contact_info TEXT
+);
+
+-- Orders Table
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE NOT NULL,
+    supplier_id INT,
+    FOREIGN KEY (supplier_id) REFERENCES Suppliers(supplier_id)
+);
+
+-- Order_Items Table
+CREATE TABLE Order_Items (
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity_ordered INT NOT NULL,
+    price_at_order_time DECIMAL(10, 2),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+       -- THE END
 
